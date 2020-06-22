6aa11c1c8c6d1372f4eb03782169284c85b5b851
vidyaannisa <vidyannisaa@gmail.com>
2020-06-22 07:53:16 +0700

    UasAppmobMultiPlatform

---
+++ src/providers/service/service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ServiceProvider {
+  API_KEY = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=2436e672d4a74d64a4568ba4256c3181';
+
+  constructor(public httpClient: HttpClient) {
+    console.log('Hello ServiceProvider Provider');
+  }
+
+  getNews(){
+    return this.httpClient.get(this.API_KEY);
+  }
+
+}
+
+
+
